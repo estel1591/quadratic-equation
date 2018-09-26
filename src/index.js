cc00ada01b4d86540e6624e6e4259371956a0a12
estel1591 <estel1591@mail.ru>
2018-09-26 21:50:56 +0300

    lovetriangle[fix]: add function to count lovetriangleslovetrianglelovetriangle[fix]: add function to count lovetriangleslovetriangle

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+    const clearEquation = equation.split(' ').join('').split('*x^2').join(',').split('*x').join(',').split(',');
+    const aEquation = clearEquation[0];
+    const bEquation = clearEquation[1];
+    const cEquation = clearEquation[2];
+    
+    const discriminant = bEquation*bEquation - 4*aEquation*cEquation;
+    let solution = [];
+    
+    if (discriminant > 0) {
+        let firstX = (-bEquation - Math.sqrt(discriminant) )/(2*aEquation);
+        let secondX = (-bEquation + Math.sqrt(discriminant) )/(2*aEquation);
+        solution.push( Math.round(firstX * 100) / 100 );
+        solution.push( Math.round(secondX * 100) / 100 );
+    }
+    
+    return solution.sort( (a, b) => a - b);
+    
 }
